994099e2a0b27ff4e6c3ae5533e8512c14367f77
Maurizio Giunti <giuntim@users.noreply.github.com>
2018-03-13 23:34:12 +0100

    New Curl method POST sample Added silent curl flag

--- command_line/ezalert-curl-post.sh
+++ command_line/ezalert-curl-post.sh
@@ -3,5 +3,5 @@
 # Replace YY-YYYYYYYYYYYY with your channel API Key
 # Remember: text parameter must be urlencoded 
 # (or at least do not use : & ? = / + and replace spaces with + )
-curl -s "https://ezalert.me/v1/sendAlert?apikey=YY-YYYYYYYYYYYY&text=Huston,+we+have+a+problem!"
+curl -s "https://ezalert.me/v1/sendAlert" -d "apikey=YY-YYYYYYYYYYYY" -d "text=Huston,+we+have+a+problem!"
 
